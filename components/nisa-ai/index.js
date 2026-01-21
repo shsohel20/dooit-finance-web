34338974ab7436b61bef35a49585be3594f05224
aimun nahar <aimunnaharr@gmail.com>
2026-01-21 14:06:32 +0600

    update

--- components/nisa-ai/index.js
+++ components/nisa-ai/index.js
@@ -1,24 +1,43 @@
 'use client';
-import { cn, getFileKind } from '@/lib/utils';
+import { cn, getFileKind, randomIdGenerator } from '@/lib/utils';
 import dynamic from 'next/dynamic';
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Button } from '../ui/button';
 import { Textarea } from '../ui/textarea';
 import { AudioLines, Cross, Forward, Mic, Paperclip, X } from 'lucide-react';
 import useOutsideClick from '@/hooks/useOutsideClick';
+import { chatWithNissa } from '@/app/actions';
+import Convos from './Convos';
 const NissaModel = dynamic(
   () => import('@/components/nisa-ai/Nisa', { ssr: false })
 );
 
+const NisaIntro = () => {
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold">
+        Hi, I&apos;m
+        <span className="bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text ml-2">
+          Nisa
+        </span>
+      </h1>
+      <p className="text-center text-sm text-gray-500">
+        Ask me anything about Risk & Compliance
+      </p>
+    </div>
+  );
+};
+
 export default function ChatBotNissa() {
   const [isOpen, setIsOpen] = useState(false);
-  console.log('isOpen', isOpen);
   const [chat, setChat] = useState([]);
   const chatRef = useRef(null);
   useOutsideClick(chatRef, () => setIsOpen(false));
   const fileInputRef = useRef(null);
   const [fileInput, setFileInput] = useState(null);
-  console.log('fileInput', fileInput);
+  const [message, setMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+  const endRef = useRef(null);
 
   const handleFileInput = (e) => {
     const file = e.target.files?.[0];
@@ -39,6 +58,72 @@ export default function ChatBotNissa() {
 
     e.target.value = null;
   };
+  const scrollToBottom = () => {
+    endRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+  useEffect(() => {
+    scrollToBottom();
+  }, [chat]);
+
+  //onenter the message should send
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSendMessage();
+    }
+  };
+  // useEffect(() => {
+  //   if (!isOpen) return;
+  //   document.addEventListener('keydown', handleKeyDown);
+  //   return () => {
+  //     document.removeEventListener('keydown', handleKeyDown);
+  //   };
+  // }, [isOpen]);
+
+  const handleSendMessage = async () => {
+    if (!message.trim()) return;
+    setMessage('');
+    console.log('message', message);
+    const myMsg = {
+      msg: message,
+      type: 'me',
+      id: randomIdGenerator(),
+      timeStamp: new Date(),
+    };
+
+    setChat((prev) => [...prev, myMsg]);
+
+    const payload = {
+      query: message,
+      session_id: 'msg',
+    };
+
+    try {
+      setLoading(true);
+      const res = await chatWithNissa(payload);
+
+      const data = res.success
+        ? {
+            msg: res.answer,
+            type: 'ai',
+            id: randomIdGenerator(),
+            timeStamp: new Date(),
+          }
+        : {
+            msg: 'Something went wrong',
+            type: 'ai',
+            id: randomIdGenerator(),
+            timeStamp: new Date(),
+            error: true,
+          };
+
+      setChat((prev) => [...prev, data]);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <div
@@ -67,19 +152,17 @@ export default function ChatBotNissa() {
         {/* chat */}
 
         <div className=" p-2  h-full flex flex-col  ">
-          {chat.length === 0 && (
-            <div className="h-full bg-white mb-2 rounded-lg p-2  overflow-y-auto ">
-              <h1 className="text-center text-2xl font-bold">
-                Hi, I&apos;m
-                <span className="bg-gradient-to-r from-primary to-accent text-transparent bg-clip-text ml-2">
-                  Nisa
-                </span>
-              </h1>
-              <p className="text-center text-sm text-gray-500">
-                Ask me anything about Risk & Compliance
-              </p>
-            </div>
-          )}
+          {/* {chat.length === 0 && ( */}
+          <div className="h-full  bg-white mb-2 rounded-lg p-2  overflow-y-auto ">
+            {chat.length === 0 && <NisaIntro />}
+            {chat.length > 0 && (
+              <div>
+                <Convos chat={chat} loading={loading} />
+                <div ref={endRef} />
+              </div>
+            )}
+          </div>
+          {/* )} */}
           <div className=" w-full border rounded-lg mt-auto bg-white ">
             <div className="h-full w-full  rounded-lg bg-gradient-to-b from-white to-zinc-100">
               {fileInput && (
@@ -137,6 +220,14 @@ export default function ChatBotNissa() {
               <Textarea
                 name="message"
                 id="message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyDown={(e) => {
+                  if (e.key === 'Enter' && !e.shiftKey) {
+                    e.preventDefault();
+                    handleSendMessage();
+                  }
+                }}
                 placeholder="Ask me anything"
                 className="resize-none  w-full border-0 shadow-none focus-visible:ring-0 focus-visible:ring-offset-0 max-h-40"
               />
@@ -161,7 +252,11 @@ export default function ChatBotNissa() {
                   </Button>
                 </div>
                 <div>
-                  <Button size="icon" variant="outline">
+                  <Button
+                    size="icon"
+                    variant="outline"
+                    onClick={handleSendMessage}
+                  >
                     <Forward />
                   </Button>
                 </div>
