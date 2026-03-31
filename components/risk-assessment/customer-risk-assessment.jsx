07f3c75e95fca5ca43550e4a45ee50fe6e60c684
aimun nahar <aimunnaharr@gmail.com>
2026-03-31 10:36:02 +0600

    update

--- components/risk-assessment/customer-risk-assessment.jsx
+++ components/risk-assessment/customer-risk-assessment.jsx
@@ -132,43 +132,45 @@ export function CustomerRiskAssessment() {
 
   const fetchRiskFactors = async () => {
     const response = await getRiskFactors();
-    console.log('response risk factors', response.data);
+    // console.log('response risk factors', response?.data);
     const countries = response.data.countries.map((item) => ({
       label: `${item.country} (score: ${item.score})`,
       value: item.country,
     }));
     setCountriesOptions(countries);
-    const customerTypes = response.data.customerType.map((item) => ({
+    const customerTypes = response.data?.customertype?.map((item) => ({
       label: `${item.value.split('_').join(' ')} (score: ${item.score})`,
       value: item.value,
     }));
     setCustomerTypesOptions(customerTypes);
-    const jurisdictions = response.data.jurisdiction.map((item) => ({
+    const jurisdictions = response.data?.jurisdiction?.map((item) => ({
       label: `${item.value} (score: ${item.score})`,
       value: item.value,
     }));
     setJurisdictionsOptions(jurisdictions);
-    const customerRetentions = response.data.customerRetention.map((item) => ({
-      label: `${item.value} (score: ${item.score})`,
-      value: item.value,
-    }));
+    const customerRetentions = response.data?.customerretention?.map(
+      (item) => ({
+        label: `${item.value} (score: ${item.score})`,
+        value: item.value,
+      })
+    );
     setCustomerRetentionsOptions(customerRetentions);
-    const products = response.data.product.map((item) => ({
+    const products = response.data?.product?.map((item) => ({
       label: `${item.value} (score: ${item.score})`,
       value: item.value,
     }));
     setProductsOptions(products);
-    const channels = response.data.channel.map((item) => ({
+    const channels = response.data?.channel?.map((item) => ({
       label: `${item.value} (score: ${item.score})`,
       value: item.value,
     }));
     setChannelsOptions(channels);
-    const occupations = response.data.occupation.map((item) => ({
+    const occupations = response.data?.occupation?.map((item) => ({
       label: `${item.value} (score: ${item.score})`,
       value: item.value,
     }));
     setOccupationsOptions(occupations);
-    const industries = response.data.industry.map((item) => ({
+    const industries = response.data?.industry?.map((item) => ({
       label: `${item.value.split('_').join(' ')} (score: ${item.score})`,
       value: item.value,
     }));
