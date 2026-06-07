cc22f42ac37ac60c5f7710a416cc6a881e89f4bd
SH SOHEL <49935124+shsohel@users.noreply.github.com>
2026-06-07 07:23:07 +0600

    update register button not clicable

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -23,7 +23,7 @@ import { toast } from 'sonner';
 const registerSchema = z
   .object({
     name: z.string().min(1, 'Name is required'),
-    userName: z.string().min(1, 'Username is required'),
+    // userName: z.string().min(1, 'Username is required'),
     email: z.string().email('Invalid email address'),
     password: z.string().min(6, 'Password must be at least 6 characters long'),
     confirmPassword: z
